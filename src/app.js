d98c23d6f18568f6d71fd9b78453cba300673be5
Adarsh <codewithadarsh007@gmail.com>
2026-03-18 12:38:54 +0530

    Advance Routing techniques

--- src/app.js
+++ src/app.js
@@ -2,33 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user",(req,res)=>{
+app.get(/.*fly$/,(req,res)=>{
  
-    res.send("HAHAHAHAHA")
+    res.send({firstName:"Peaky", lastName: "Blinders"})
 
 })
-
-app.get("/user",(req,res)=>{
- 
-    res.send({firstName:"Peaky", lastName: "Samal"})
-
-})
-
-app.post("/user",(req,res)=>{
- 
-    res.send("Data Successfully saved to DB");
-
-})
-
-app.delete("/user",(req,res)=>{
- 
-    res.send("Deleted Successfully");
-
-})
-
-// app.listen(7777);
-
-// console.log("Server successfully connected");
 
 app.listen(7777,()=>{
   
@@ -36,17 +14,48 @@ app.listen(7777,()=>{
 
 })
 
+
+// app.use("/user",(req,res)=>{
+ 
+//     res.send("HAHAHAHAHA")
+
+// })
+
+// app.get("/user",(req,res)=>{
+ 
+//     res.send({firstName:"Peaky", lastName: "Samal"})
+
+// })
+
+// app.post("/user",(req,res)=>{
+ 
+//     res.send("Data Successfully saved to DB");
+
+// })
+
+// app.delete("/user",(req,res)=>{
+ 
+//     res.send("Deleted Successfully");
+
+// })
+
+// app.listen(7777);
+
+// console.log("Server successfully connected");
+
+
+
 // app.use((req,res)=>{
   
 //     res.send("Hello from the server");
 
 // })
 
-app.use("/test",(req,res)=>{
+// app.use("/test",(req,res)=>{
   
-    res.send("Let's Test");
+//     res.send("Let's Test");
 
-})
+// })
 
 // app.use("/",(req,res)=>{
   
@@ -54,11 +63,11 @@ app.use("/test",(req,res)=>{
 
 // })
 
-app.use("/Bye",(req,res)=>{
+// app.use("/Bye",(req,res)=>{
   
-    res.send("Byee");
+//     res.send("Byee");
 
-})
+// })
 
 
 
