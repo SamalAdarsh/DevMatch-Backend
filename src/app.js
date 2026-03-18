c09451a479aa18ad04cbfa31e68cb871a5b321c9
Adarsh <codewithadarsh007@gmail.com>
2026-03-18 22:23:24 +0530

    Middleware

--- src/app.js
+++ src/app.js
@@ -2,39 +2,96 @@ const express = require("express");
 
 const app = express();
 
-app.get(/.*fly$/,(req,res)=>{
- 
-    res.send({firstName:"Peaky", lastName: "Blinders"})
+// app.get(/.*fly$/,(req,res)=>{
 
-})
+//     res.send({firstName:"Peaky", lastName: "Blinders"})
 
-app.listen(7777,()=>{
-  
-    console.log("Server successfully connected");
+// })
 
-})
+// app.use("/user", (req,res,next)=>{
+//     console.log("Handle 1");
+//      next();
+//     // res.send("Respone 1");
+// },
+// [
+// (req,res,next)=>{
+//     console.log("Handle 2");
+//     // res.send("Respone 2");
+//       next();
+// },
+// (req,res,next)=>{
+//     console.log("Handle 3");
+//     // res.send("Respone 2");
+//       next();
+// }],
+// (req,res,next)=>{
+//     console.log("Handle 4");
+//     // res.send("Respone 2");
+//       next();
+// },
+// (req,res,next)=>{
+//     console.log("Handle 5");
+//     res.send("Respone 5");
+//       next();
+// }
 
+// )
+
+app.use("/", (req, res, next) => {
+  console.log("Handle/route");
+  // res.send("Response")
+  next();
+});
+
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("Handle 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handle 2");
+    res.send("Response 2");
+  },
+);
+
+// app.get("/user",(req,res,next)=>{
+
+//     console.log("Handle 2");
+//    res.send("Response 2");
+// })
+
+// app.get("/user/:userId/:name/:password",(req,res)=>{
+
+//     console.log(req.params)
+//     res.send({firstName:"Peaky", lastName: "Blinders"})
+
+// })
+
+app.listen(7777, () => {
+  console.log("Server successfully connected");
+});
 
 // app.use("/user",(req,res)=>{
- 
+
 //     res.send("HAHAHAHAHA")
 
 // })
 
 // app.get("/user",(req,res)=>{
- 
+
 //     res.send({firstName:"Peaky", lastName: "Samal"})
 
 // })
 
 // app.post("/user",(req,res)=>{
- 
+
 //     res.send("Data Successfully saved to DB");
 
 // })
 
 // app.delete("/user",(req,res)=>{
- 
+
 //     res.send("Deleted Successfully");
 
 // })
@@ -43,31 +100,26 @@ app.listen(7777,()=>{
 
 // console.log("Server successfully connected");
 
-
-
 // app.use((req,res)=>{
-  
+
 //     res.send("Hello from the server");
 
 // })
 
 // app.use("/test",(req,res)=>{
-  
+
 //     res.send("Let's Test");
 
 // })
 
 // app.use("/",(req,res)=>{
-  
+
 //     res.send("Adarsh");
 
 // })
 
 // app.use("/Bye",(req,res)=>{
-  
+
 //     res.send("Byee");
 
 // })
-
-
-
