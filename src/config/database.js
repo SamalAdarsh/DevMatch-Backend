1e32cca0d8a3990e2ff857e0a2131e4fcadd0231
Adarsh <codewithadarsh007@gmail.com>
2026-03-20 17:24:45 +0530

    Created userSchema

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb://codewithadarsh007_db_user:j91q2M54eWZEuke5@ac-muoulls-shard-00-00.hdkffxs.mongodb.net:27017,ac-muoulls-shard-00-01.hdkffxs.mongodb.net:27017,ac-muoulls-shard-00-02.hdkffxs.mongodb.net:27017/?ssl=true&replicaSet=atlas-zrd0bj-shard-0&authSource=admin&appName=Cluster0"
+    "mongodb://codewithadarsh007_db_user:j91q2M54eWZEuke5@ac-muoulls-shard-00-00.hdkffxs.mongodb.net:27017,ac-muoulls-shard-00-01.hdkffxs.mongodb.net:27017,ac-muoulls-shard-00-02.hdkffxs.mongodb.net:27017/devMatch?ssl=true&replicaSet=atlas-zrd0bj-shard-0&authSource=admin&appName=Cluster0"
   );
 };
 
