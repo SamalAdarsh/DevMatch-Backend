cfbbe8adfef2da7d6dbe75b5e4ef656c1c94a593
Adarsh <codewithadarsh007@gmail.com>
2026-03-25 19:34:44 +0530

    Router for each API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const {userAuth} = require("../middlewares/auth");
+const { userAuth } = require("../middlewares/auth");
+const {
+  validateProfileEditData,
+  validatePasswordUpdateData,
+} = require("../utils/validate");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 const profileRouter = express.Router();
 
-profileRouter.get("/profile",userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     // const cookies = req.cookies;
 
@@ -18,16 +24,57 @@ profileRouter.get("/profile",userAuth, async (req, res) => {
     // console.log("Logged in user is :" + _id);
 
     const user = req.user;
-    
-    if(!user) throw new Error("User does not exist");
-   res.send(user);
+
+    if (!user) throw new Error("User does not exist");
+    res.send(user);
 
     // console.log(cookies);
-   
   } catch (err) {
+    res.status(400).send("ERROR:" + err.message);
+  }
+});
 
-     res.status(400).send("ERROR:" + err.message);
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateProfileEditData(req)) throw new Error("Invalid edit request");
 
+    const loggedInUser = req.user;
+
+    Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+
+    await loggedInUser.save();
+
+    //   res.send(`${loggedInUser.firstName}, has successfully updated their profile`);
+
+    res.json({
+      message: `${loggedInUser.firstName}, has successfully updated their profile`,
+      data: loggedInUser,
+    });
+  } catch (err) {
+    res.status(400).send("Error:" + err.message);
+  }
+});
+
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+  try {
+    validatePasswordUpdateData(req);
+
+    const { oldPassword, newPassword } = req.body;
+
+    const user = await User.findById(req.user._id).select("+password");
+
+    const isPasswordMatch = await bcrypt.compare(oldPassword, user.password);
+    if (!isPasswordMatch) {
+      throw new Error("Invalid current password");
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    user.password = hashedPassword;
+    await user.save();
+
+    res.json({ message: "Password updated successfully!" });
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
   }
 });
 
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,20 +1,61 @@
 const validator = require("validator");
 
-const validateSignUpData = (req)=>{
+const validateSignUpData = (req) => {
+  const { firstName, lastName, emailId, password } = req.body;
 
-    const{firstName,lastName,emailId, password} = req.body;
+  if (!firstName || !lastName) {
+    throw new Error("Enter the valid name");
+  } else if (!validator.isEmail(emailId)) {
+    throw new Error("Enter a valid Email");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Enter a valid password");
+  }
+};
 
-    if(!firstName || !lastName){
-        throw new Error("Enter the valid name");
-    }
-    else if(!validator.isEmail(emailId)){
-         throw new Error("Enter a valid Email");
-    }
-       else if(!validator.isStrongPassword(password)){
-         throw new Error("Enter a valid password");
-    }
+const validateProfileEditData = (req) => {
+  if (req.body?.skills?.length > 10) {
+    throw new Error("Only max 10 skills allowed");
+  }
+  const allowedEditFields = [
+    "firstName",
+    "lastName",
+    "age",
+    "gender",
+    "about",
+    "photoURL",
+    "skills",
+  ];
+
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowedEditFields.includes(field),
+  );
+
+  return isEditAllowed;
+};
+
+const validatePasswordUpdateData = (req) => {
+  const { oldPassword, newPassword } = req.body;
+
+ 
+  if (!oldPassword || !newPassword) {
+    throw new Error("Both current and new passwords are required");
+  }
+
+
+
+  if (!validator.isStrongPassword(newPassword, { minLength: 10 })) {
+    throw new Error(
+      "New password is too weak! Use 10+ chars with uppercase, numbers, and symbols.",
+    );
+  }
+
+  if (oldPassword === newPassword) {
+    throw new Error("New password cannot be the same as the current password");
+  }
 };
 
 module.exports = {
-    validateSignUpData,
-}
+  validateSignUpData,
+  validateProfileEditData,
+  validatePasswordUpdateData,
+};
