03c74303e685e493780dac07f146b9cd297092ad
Adarsh <codewithadarsh007@gmail.com>
2026-03-26 11:57:16 +0530

    Connection Request Schema

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,17 +1,37 @@
 const express = require("express");
 const {userAuth} = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
 
 
 const requestRouter = express.Router();
 
 
 
-requestRouter.post("/sendConnectRequest", userAuth ,async (req, res) => {
+requestRouter.post("/request/send/:status/:toUserId", userAuth ,async (req, res) => {
 
-  const user = req.user;
-  console.log("Connection Request Sent");
+    try{
+            const fromUserId = req.user._id;
+            const toUserId = req.params.toUserId;
+            const status = req.params.status;
 
-  res.send(user.firstName + " sent a connect request");
+        const connectionRequest = new ConnectionRequest({
+            fromUserId,
+            toUserId,
+            status,
+        })
+
+        const data = await connectionRequest.save();
+
+        res.json({
+            message: "Connection Request Sent Successfully",
+            data,
+        })
+    }
+    catch(err){
+
+        res.status(400).send("Error:" + err.message);
+    }
+  
   
 });
 
