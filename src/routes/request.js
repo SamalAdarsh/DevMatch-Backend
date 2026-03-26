40d12b6a244f2ccf60b790539d3e0ad4fba352a8
Adarsh <codewithadarsh007@gmail.com>
2026-03-26 12:58:29 +0530

    Handled Corner Cases

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const {userAuth} = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
+const { connection } = require("mongoose");
 
 
 const requestRouter = express.Router();
@@ -14,11 +16,52 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth ,async (req, res)
             const toUserId = req.params.toUserId;
             const status = req.params.status;
 
+            // Corner Case: 1 (only send ignored || interested status in connection request)
+
+            const allowedStatus = ["ignored","interested"];
+
+            if(!allowedStatus.includes(status)){
+
+                return res.status(400).json({
+                    message: "Invalid status type -> " + status,
+                })
+            }
+
+            //Corner Case: 2 (Avoid sending request to existing connection)
+
+            const existingConnnectionRequest = await ConnectionRequest.findOne({
+
+                $or: [
+                    {fromUserId,toUserId},
+                    {fromUserId : toUserId,toUserId : fromUserId},
+                ]
+            })
+
+            if(existingConnnectionRequest){
+              return  res.status(400).send("Already sent conection request");
+            }
+
+            //Corner Case: 3 (Prevent sending request to user who are not present in the DB);
+
+            const toUser = await User.findById(toUserId);
+
+            if(!toUser){
+              return  res.status(400).send("User not found");
+            }
+
         const connectionRequest = new ConnectionRequest({
             fromUserId,
             toUserId,
             status,
         })
+
+        //Corner Case: 4 (Cannot send request to yourself)
+
+        // if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+
+        //     return res.status(400).send("Cannot send request to yourself");
+
+        // }
 
         const data = await connectionRequest.save();
 
